ed024f1f9b4830abdee39e55c0f38d43d0e53096
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2022-06-05 15:44:38 -0300

    Ensure AddBookRouter returns 400 if no photo is provided

--- src/presentation/add-book-router.spec.js
+++ src/presentation/add-book-router.spec.js
@@ -1,6 +1,6 @@
 class AddBookRouter {
   route(httpRequest) {
-    if (!httpRequest.body.title || !httpRequest.body.publisher) {
+    if (!httpRequest.body.title || !httpRequest.body.publisher || !httpRequest.body.photo) {
       return {
         statusCode: 400
       }
@@ -32,6 +32,22 @@ describe('AddBookRouter', () => {
       body: {
         title: 'any title',
         photo: 'any photo',
+        authors: ['any author']
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  it('Should return 400 if no photo is provided', () => {
+    const sut = new AddBookRouter()
+
+    const httpRequest = {
+      body: {
+        title: 'any title',
+        publisher: 'any publisher',
         authors: ['any author']
       }
     }
