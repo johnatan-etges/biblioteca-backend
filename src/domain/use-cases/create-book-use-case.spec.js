67e27d6e8dd551f8a83c0dbd3fba35b0187d5918
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2022-06-08 21:39:58 -0300

    Ensure CreateBookUseCase throws custom errror

--- src/domain/use-cases/create-book-use-case.spec.js
+++ src/domain/use-cases/create-book-use-case.spec.js
@@ -1,7 +1,19 @@
+const { MissingParamError } = require("../../shared/errors")
+
 class CreateBookUseCase{
   async execute(title, publisher, photo, authors) {
-    if (!title || !publisher || !photo || !authors) {
-      throw new Error()
+    
+    if (!title) {
+      throw new MissingParamError('title')
+    }
+    if (!publisher) {
+      throw new MissingParamError('publisher')
+    }
+    if (!photo) {
+      throw new MissingParamError('photo')
+    }
+    if (!authors) {
+      throw new MissingParamError('authors')
     }
   }
 }
@@ -11,25 +23,25 @@ describe('CreateBookUseCase', () => {
   it('Should throw if no title is provided', async () => {
     const sut = new CreateBookUseCase()
     const promisse = sut.execute()
-    await expect(promisse).rejects.toEqual(new Error())
+    await expect(promisse).rejects.toEqual(new MissingParamError('title'))
   })
 
   it('Should throw if no publisher is provided', async () => {
     const sut = new CreateBookUseCase()
     const promisse = sut.execute('any title')
-    await expect(promisse).rejects.toEqual(new Error())
+    await expect(promisse).rejects.toEqual(new MissingParamError('publisher'))
   })
 
   it('Should throw if no photo is provided', async () => {
     const sut = new CreateBookUseCase()
     const promisse = sut.execute('any title', 'any publisher')
-    await expect(promisse).rejects.toEqual(new Error())
+    await expect(promisse).rejects.toEqual(new MissingParamError('photo'))
   })
 
   it('Should throw if no author is provided', async () => {
     const sut = new CreateBookUseCase()
     const promisse = sut.execute('any title', 'any publisher', 'any photo')
-    await expect(promisse).rejects.toEqual(new Error())
+    await expect(promisse).rejects.toEqual(new MissingParamError('authors'))
   })
 
 })
