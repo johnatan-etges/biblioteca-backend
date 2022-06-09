f38fc2b2fbf357fb77196fed3d68334b6251bb98
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2022-06-08 21:21:59 -0300

    Ensure CreateBookUseCase throws if no photo is provided

--- src/domain/use-cases/create-book-use-case.spec.js
+++ src/domain/use-cases/create-book-use-case.spec.js
@@ -1,6 +1,6 @@
 class CreateBookUseCase{
-  async execute(title, publisher) {
-    if (!title || !publisher) {
+  async execute(title, publisher, photo) {
+    if (!title || !publisher || !photo) {
       throw new Error()
     }
   }
@@ -17,6 +17,12 @@ describe('CreateBookUseCase', () => {
   it('Should throw if no publisher is provided', async () => {
     const sut = new CreateBookUseCase()
     const promisse = sut.execute('any title')
+    await expect(promisse).rejects.toEqual(new Error())
+  })
+
+  it('Should throw if no photo is provided', async () => {
+    const sut = new CreateBookUseCase()
+    const promisse = sut.execute('any title', 'any publisher')
     await expect(promisse).rejects.toEqual(new Error())
   })
 
