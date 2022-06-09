99dc800d893b535d688b9914c1bad90edcf30a7a
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2022-06-08 21:20:54 -0300

    Ensure CreateBookUseCase throws if no publisher is provided

--- src/domain/use-cases/create-book-use-case.spec.js
+++ src/domain/use-cases/create-book-use-case.spec.js
@@ -1,15 +1,23 @@
 class CreateBookUseCase{
-  async execute(title) {
-    if (!title) {
+  async execute(title, publisher) {
+    if (!title || !publisher) {
       throw new Error()
     }
   }
 }
 
 describe('CreateBookUseCase', () => { 
+
   it('Should throw if no title is provided', async () => {
     const sut = new CreateBookUseCase()
     const promisse = sut.execute()
-    expect(promisse).rejects.toEqual(new Error())
+    await expect(promisse).rejects.toEqual(new Error())
   })
+
+  it('Should throw if no publisher is provided', async () => {
+    const sut = new CreateBookUseCase()
+    const promisse = sut.execute('any title')
+    await expect(promisse).rejects.toEqual(new Error())
+  })
+
 })
