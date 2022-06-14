fd11a22db9852f7e653b2ad6de6c13635e838e1b
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2022-06-14 20:59:44 -0300

    Ensure CreateBookUseCase throws if an invalid FindBookByTitleRepository is provided

--- src/domain/use-cases/create-book-use-case.spec.js
+++ src/domain/use-cases/create-book-use-case.spec.js
@@ -1,8 +1,11 @@
-const { MissingParamError } = require("../../shared/errors")
+const { MissingParamError, InvalidParamError } = require("../../shared/errors")
 
 class CreateBookUseCase{
-  async execute(title, publisher, photo, authors) {
-    
+  constructor(findBookByTitleRepository) {
+    this.findBookByTitleRepository = findBookByTitleRepository
+  }
+
+  async execute(title, publisher, photo, authors) {    
     if (!title) {
       throw new MissingParamError('title')
     }
@@ -15,33 +18,66 @@ class CreateBookUseCase{
     if (!authors) {
       throw new MissingParamError('authors')
     }
+
+    if (!this.findBookByTitleRepository) {
+      throw new MissingParamError('findBookByTitleRepository')
+    }
+
+    if (!this.findBookByTitleRepository.find) {
+      throw new InvalidParamError('findBookByTitleRepository')
+    }
+  }
+}
+class FindBookByTitleRepositorySpy {
+
+}
+
+const makeSut = () => {
+  const findBookByTitleRepositorySpy = new FindBookByTitleRepositorySpy()
+  const sut = new CreateBookUseCase(findBookByTitleRepositorySpy)
+  return {
+    sut,
+    findBookByTitleRepositorySpy
   }
 }
 
 describe('CreateBookUseCase', () => { 
 
   it('Should throw if no title is provided', async () => {
-    const sut = new CreateBookUseCase()
+    const { sut } = makeSut()
     const promisse = sut.execute()
     await expect(promisse).rejects.toEqual(new MissingParamError('title'))
   })
 
   it('Should throw if no publisher is provided', async () => {
-    const sut = new CreateBookUseCase()
+    const { sut } = makeSut()
     const promisse = sut.execute('any title')
     await expect(promisse).rejects.toEqual(new MissingParamError('publisher'))
   })
 
   it('Should throw if no photo is provided', async () => {
-    const sut = new CreateBookUseCase()
+    const { sut } = makeSut()
     const promisse = sut.execute('any title', 'any publisher')
     await expect(promisse).rejects.toEqual(new MissingParamError('photo'))
   })
 
   it('Should throw if no author is provided', async () => {
-    const sut = new CreateBookUseCase()
+    const { sut } = makeSut()
     const promisse = sut.execute('any title', 'any publisher', 'any photo')
     await expect(promisse).rejects.toEqual(new MissingParamError('authors'))
   })
 
+  it('Should throw if no FindBookByTitleRepository is provided', async () => {
+    const sut = new CreateBookUseCase()
+    const promisse = sut.execute('any title', 'any publisher', 'any photo', ['any author'])
+    await expect(promisse).rejects.toEqual(new MissingParamError('findBookByTitleRepository'))
+  })
+
+
+  it('Should throw if FindBookByTitleRepository has no find method', async () => {
+    const invalidFindBookByTitleRepository = {}
+    const sut = new CreateBookUseCase(invalidFindBookByTitleRepository)
+    const promisse = sut.execute('any title', 'any publisher', 'any photo', ['any author'])
+    await expect(promisse).rejects.toEqual(new InvalidParamError('findBookByTitleRepository'))
+  })
 })
--- src/shared/errors/index.js
+++ src/shared/errors/index.js
@@ -1,4 +1,5 @@
 const MissingParamError = require('./missing-param-error')
 const ResourceConflictError = require('./resource-conflict-error')
+const InvalidParamError = require('./invalid-param-error')
 
-module.exports = { MissingParamError, ResourceConflictError }
+module.exports = { MissingParamError, ResourceConflictError, InvalidParamError }
